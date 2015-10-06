2f8d12756e5e3320db38aacc4d1035d3fc9a4bcc
Blaž Oražem <blaz@orazem.info>
2015-10-06 22:48:29 +0200

    Ep03 - Basic Gulp Housekeeping

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Project Flyer</title>
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
