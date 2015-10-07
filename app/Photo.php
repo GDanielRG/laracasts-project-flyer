7daba158962eaff6f60c01841400fd126ee40898
Blaž Oražem <blaz@orazem.info>
2015-10-07 20:28:06 +0200

    Ep07 - The Flyer and Photos Relationship

---
+++ app/Photo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $table = 'flyer_photos';
+
+    protected $fillable = ['photo'];
+
+    /**
+     * A flyer belongs to photo.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function flyer()
+    {
+        return $this->belongsTo('App\Flyer');
+    }
+
+}
