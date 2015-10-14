f58f6c3e689b3d2b5fe0db02be127bec86b7a4dc
Blaž Oražem <blaz@orazem.info>
2015-10-14 13:51:05 +0200

    Ep10 - Preparing the Flyer Page

--- app/Flyer.php
+++ app/Flyer.php
@@ -22,6 +22,32 @@ class Flyer extends Model
     ];
 
     /**
+     * Scope query to those located at a given address.
+     *
+     * @param Builder $query
+     * @param string $zip
+     * @param string $street
+     * @return Builder
+     */
+    public function scopeLocatedAt($query, $zip, $street)
+    {
+        $street = str_replace('-', ' ', $street);
+
+        return $query->where(compact('zip', 'street'));
+    }
+
+    /**
+     * Format flyer price.
+     *
+     * @param $price
+     * @return string
+     */
+    public function getPriceAttribute($price)
+    {
+        return '$' . number_format($price);
+    }
+
+    /**
      * A flyer is composed of many photos.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
